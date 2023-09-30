f1398117ca2b9aeab62641811e1e937aa96ebeb6
Kőszegi Balázs <kbalazs@Koszegi-MacBook-Air.local>
2023-09-30 19:49:03 +0200

    Add weigthed sum

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -7,11 +7,19 @@ Created on Sat Sep 30 18:11:36 2023
 """
 
 class Perceptron:
-    def __init__(self, num_inputs=2, weights=[1,1]):
+    def __init__(self, num_inputs=2, weights=[2,1]):
         self.num_inputs = num_inputs
         self.weights = weights
+    
+    def weigthed_sum(self, inputs):
+        #create variable to store weighted sum
+        weigthed_sum = 0
+        for i in range(self.num_inputs):
+            weigthed_sum += self.weights[i] * inputs[i]
+            
+        return weigthed_sum
 
 # Creating the first instance of the Perceptron class (object)
 cool_perceptron = Perceptron()
-print(cool_perceptron)
+print(cool_perceptron.weigthed_sum([24, 55]))
         
